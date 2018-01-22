21d070d9429c2ca8a9c9c91d7810c66d2603ef20
Kashyap Sharma <kashyap.sharma@imaginea.com>
2018-01-22 22:28:23 +0530

    checkstyle bug fixes

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,6 +12,10 @@ public class Greeter {
 
   }
 
+  /**
+   * @param  hello to a person
+   * @return greeting string
+   */ 
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
